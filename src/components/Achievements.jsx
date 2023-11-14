cb29b371b4bb38863863848f9d8905e0ef49a514
TravisH <hurst_travis@yahoo.com>
2023-11-14 17:31:49 -0500

    Achievment Dialog update

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,11 +1,12 @@
 // WE ARE SO BACK YEEEEEEEAH
 import React, { useState } from 'react'
 import './Achievements.css'
-import Popup from 'reactjs-popup';
+import Popup2 from 'reactjs-popup';
 import {Tooltip} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import earnedAward from '../assets/images/earned-award.png'
+import unearnedAward from '../assets/images/unearned-award.png'
 
 const ProgressBarButCute = styled(LinearProgress)(({ theme }) => (
     {
@@ -14,33 +15,43 @@ const ProgressBarButCute = styled(LinearProgress)(({ theme }) => (
         [`& .${linearProgressClasses.bar}`]: {borderRadius: 5}
     }));
 
-function AchievementTile({awardName, /*progress,*/ description, /*bounty,*/}) {
-    const [progress, setProgress] = useState( Math.floor(Math.random() * 100) + 1 );
+const AchievementTile = ({awardName, awardProgress, description}) => {
 
-    awardName='Example Award Name'
-    description='This is an example award description.'
+    let isComplete = -1
+    let awardIMGsource = unearnedAward
+
+    if (awardProgress == 100) {
+        isComplete = 100
+        awardIMGsource = earnedAward
+    } else { 
+        isComplete = (Math.floor(Math.random() * 100) + 1) 
+        awardIMGsource = unearnedAward
+    }
+
+    const [progress, setProgress] = useState( isComplete );
+
+    //awardName='Example Award Name'
+    //description='This is an example award description.'
 
     return (
         <div className='achievement-tile'>
-            <Popup trigger= {
+            <Popup2 trigger= {
                 <button className="award">
                     <h4>{awardName}</h4>
-                    <img src={earnedAward} alt="..." width="100%" height="auto" style={{objectFit: 'contain'}}></img>
+                    <img src={awardIMGsource} alt="..." width="100%" height="auto" style={{objectFit: 'contain'}}></img>
                 </button>
                 } modal>
-                <div className='award-popup'>
-                    <p>{description}</p>
-                    <label htmlFor='progress-bar'><i>Progress: </i></label>
-                    <Tooltip
-                        className='tooltip'
-                        title={`${progress}% complete`}
-                        followCursor={true}
-                        arrow
-                    >
-                        <ProgressBarButCute variant="determinate" value={progress} />
-                    </Tooltip>
-                </div>
-            </Popup>
+                <p>{description}</p>
+                <label htmlFor='progress-bar'><i>Progress: </i></label>
+                <Tooltip
+                    className='tooltip'
+                    title={`${progress}% complete`}
+                    followCursor={true}
+                    arrow
+                >
+                    <ProgressBarButCute variant="determinate" value={progress} />
+                </Tooltip>
+            </Popup2>
 
         </div>
     )
@@ -49,12 +60,11 @@ function AchievementTile({awardName, /*progress,*/ description, /*bounty,*/}) {
 
 
 export const Achievements = ({openModalAchievements, setOpenAchievements}) => {
-    /*const clickRight = () => {
-
-    }
-    const clickLeft = () => {
-        
-    }*/
+    const [currentShelf, setCurrentShelf] = useState(1);
+    
+    const clickRight = () => {setCurrentShelf(2); }
+    
+    const clickLeft = () => {setCurrentShelf(1); }
 
     return( 
         <div className='popup-background'>
@@ -62,21 +72,45 @@ export const Achievements = ({openModalAchievements, setOpenAchievements}) => {
 
             <div className='popup-body'>
             
-                <button className='leftArrow'>
+                <button className='leftArrow' onClick={clickLeft}>
                     <span>&#8592;</span>
                 </button>
 
-                <button className='rightArrow'>
+                <button className='rightArrow' onClick={clickRight}>
                     <span >&#8594;</span>
                 </button>
 
-                <div className='topShelf'>
-                    <AchievementTile/>
-                    <AchievementTile/>
+                <div className='shelf shelf1' >
+                    {(currentShelf===1) && (
+                        <>
+                        <AchievementTile
+                            awardName="Conquer Computer Graphics"
+                            awardProgress={100}
+                            description={'Complete all assignments due for Computer Graphics course.'}
+                        />
+                        <AchievementTile
+                            awardName="UI Expedition"
+                            awardProgress={-1}
+                            description={'Complete all assignments due for UI course.'}
+                        />
+                        </>
+                    )}
                 </div>
-                <div className='bottomShelf'>
-                    <AchievementTile/>
-                    <AchievementTile/>
+                <div className='shelf shelf2'>
+                    {(currentShelf===2) && (
+                        <>
+                        <AchievementTile
+                            awardName="Senior Design Odyssey"
+                            awardProgress={-1}
+                            description={'Complete all tasks for Senior Design, and attend CEAS Expo.'}
+                        />
+                        <AchievementTile
+                            awardName="Quest for the Golden Transcript"
+                            awardProgress={-1}
+                            description={'Maintain a GPA of 3.0 or higher.'}
+                        />
+                        </>
+                    )}
                 </div>
                 <button 
                     className='modal-footer-button modal-button-cancel' 
